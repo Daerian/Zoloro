f5a6903a83ec97d7ddd3ac6432db7741b35db4e4
Daerian Dilkumar <daerian.dilkumar@hotmail.com>
2018-01-16 14:58:34 -0500

    Added 2 classes.
    Added a single commands class. so far only contains passive events. Added Constants class as well.
    
     need to do more.

---
+++ src/main/java/Zoloro/Zoloro/Constants.java
@@ -0,0 +1,9 @@
+package Zoloro.Zoloro;
+
+// A class just to constants to refer to, such as the token for this bot or client IDs for servers
+// #GoodFormatting
+public class Constants {
+	
+	// Constants go here
+	public static final String TOKEN = "NDAxNTcwNDU4NjM5NTMyMDMz.DTxnLg.LtD7sUT-thEtHTlZV69Dg5NmbFM";
+}
